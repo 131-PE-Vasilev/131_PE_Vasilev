44bc192ef0aee8e9cfdf7e5aac40772063940762
AnastasiyaR <mastusha2603@gmail.com>
2016-02-08 12:36:49 +0300

    Лабораторная 1 задание 1 оплдата дневной выручки

--- 131_PE_Vasilev/Program.cs
+++ 131_PE_Vasilev/Program.cs
@@ -10,6 +10,14 @@ namespace _131_PE_Vasilev
     {
         static void Main(string[] args)
         {
+            int v;
+            Console.WriteLine("Введите размер дневной выручки продавца");
+            v = Convert.ToInt32(Console.ReadLine());
+            if (v <= 10000)
+                Console.WriteLine("Выручка продавца");
+            else
+                Console.WriteLine("Выручка продавца = {0}", 500 + ((v-10000)*0.1));
+            Console.ReadKey();
         }
     }
 }
