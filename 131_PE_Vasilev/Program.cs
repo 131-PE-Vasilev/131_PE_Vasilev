9c9285b1fffb2272abd8d43231e10296d942960a
Vasilev <vasvova95@mail.ru>
2016-02-08 13:02:20 +0300

    лабораторная работа 1 задание 2 мин.оплата

--- 131_PE_Vasilev/Program.cs
+++ 131_PE_Vasilev/Program.cs
@@ -13,10 +13,14 @@ namespace _131_PE_Vasilev
             int v;
             Console.WriteLine("Введите размер дневной выручки продавца");
             v = Convert.ToInt32(Console.ReadLine());
-            if (v <= 10000)
-                Console.WriteLine("Выручка продавца");
+            int min = 300;
+            if (500* v <min)
+                Console.WriteLine("Выручка продавца = {0}",min);
             else
-                Console.WriteLine("Выручка продавца = {0}", 500 + ((v-10000)*0.1));
+                if (v <= 10000)
+                Console.WriteLine("Выручка продавца = {0}", 500 * v);
+            else
+                Console.WriteLine("Выручка продавца = {0}", 500 + ((v - 10000) * 0.1));
             Console.ReadKey();
         }
     }
